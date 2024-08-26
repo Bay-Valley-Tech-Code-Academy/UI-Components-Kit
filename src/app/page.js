1ad26d54e5d19d925f532fe1a6468837cf3a306d
AlissonRoss <alirosssalazar@gmail.com>
2024-08-26 14:27:20 -0700

    Removed more old code that came about from merge

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,34 @@
 'use client'
-import Image from "next/image";
-import Box from "./components/Box/box";
+
 import Navbar from "./components/navbar";
 import { useRouter } from "next/navigation";
-
 
 export default function Home() {
   const router = useRouter();
   const text = "Empowering developers for rapid innovation.";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Hero Section */}
-      <div className="mt-16 text-center">
-        <h1 className="pl-6 text-4xl sm:text-5xl md:text-7xl xl:text-8xl font-extrabold">
-          {text.split("").map((letter, index) => (
-            <span
-              key={index}
-              className="transition-colors duration-300 ease-out hover:text-[#FF5531] text-white"
-            >
-              {letter}
-            </span>
-          ))}
-        </h1>
+    <div>
+      <Navbar/>
+      <div>
+        {/* Created by developers for developers. */}
+        <h1 className="pl-6 text-4xl sm:text-5xl md:text-7xl xl:text-8xl mt-16 font-extrabold">{text.split("").map((letter, index) => (
+                <span key={index} className="transition-colors duration-300 ease-out hover:text-[#FF5531] text-white">{letter}</span>
+            ))}</h1>
       </div>
-
-      {/* Buttons Section */}
-      <div className="flex items-center justify-center mt-8">
-        <button
-          className="text-white p-2 text-base md:text-2xl bg-[#FF5531] hover:bg-[#CC4426] rounded-md m-2 hover:translate-y-1"
+      <div className="flex items-center justify-center">
+        <div>
+          <button className="text-white p-2 text-base md:text-2xl bg-[#FF5531] hover:bg-[#CC4426] rounded-md m-2 hover:translate-y-1"
           onClick={() => router.push("/template")}
-        >
-          Browse templates
-        </button>
-        <button
-          className="text-white p-2 text-base md:text-2xl bg-[#FF5531] hover:bg-[#CC4426] rounded-md m-2 hover:translate-y-1"
+          >Browse templates</button>
+        </div>
+        <div>
+          <button className="text-white p-2 text-base md:text-2xl bg-[#FF5531] hover:bg-[#CC4426] rounded-md m-2 hover:translate-y-1"
           onClick={() => router.push("/component")}
-        >
-          Browse components
-        </button>
-        <img
-          src="/images/bvt_logo.png"
-          alt="BVT Logo"
-          className="w-1/4 transition duration-300 ease-out hover:scale-125"
-        />
+          >Browse components</button>
+        </div>
+        <img src="/images/bvt_logo.png" alt="" className="w-1/4 transition duration-300 ease-out hover:scale-125"/>
       </div>
-    </main>
+    </div>
   );
 }
