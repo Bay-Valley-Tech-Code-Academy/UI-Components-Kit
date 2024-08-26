2dd0794caab3e8712f32a764373b51e343a0352a
AlissonRoss <alirosssalazar@gmail.com>
2024-08-26 14:01:49 -0700

    Dynamically sends over styles as props to the Box component

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,14 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Box className="bg-midnight"/>
+       <Box 
+          description="Explore Tailwind CSS" 
+          title="Tailwind Components" 
+          button="Learn More" 
+          p="By John Doe" 
+          buttonColor="bg-pink-600"
+          hoverColor= "hover:bg-orange-500"
+        />
       </div>
     </main>
   );
