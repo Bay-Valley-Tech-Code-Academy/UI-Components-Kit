7e798b65427e16313aca3a432854514b4f4967d3
Misael Bolainez <misaelbolainez@gmail.com>
2024-08-26 16:52:41 -0700

    feat(init layout): Created the initial layout for the components page

--- src/app/component/page.js
+++ src/app/component/page.js
@@ -1,10 +1,51 @@
+"use client";
+
 import Navbar from "../components/navbar";
-import Box from "../components/Box/box";
-export default function Home() {
+import ComponentCard from "../components/componentCard";
+import NavbarList from "../components/navbarList";
+import { useState } from "react";
+
+export default function Component() {
+  const [selectedContent, setSelectedContent] = useState(null);
+
+  const cards = [
+    { title: "Navbar", content: <NavbarList /> },
+    { title: "Footer", content: "" },
+    { title: "Promo", content: "" },
+    { title: "Card", content: "" },
+  ];
+
+  const handleCardClick = (content) => {
+    setSelectedContent(content);
+  };
+
   return (
     <div className="">
-      <Navbar/>
-       {/* Grid of Boxes */}
+      <Navbar />
+      <div className="flex justify-start p-6 text-white text-4xl sm:text-5xl md:text-7xl xl:text-8xl mt-10 font-extrabold mb-10">
+        <h1>Components</h1>
+      </div>
+      {selectedContent === null ? (
+        <div className="flex flex-wrap justify-around p-6">
+          {cards.map((card, index) => (
+            <ComponentCard
+              key={index}
+              title={card.title}
+              onClick={() => handleCardClick(card.content)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col p-6">
+          <div>
+            <button className="text-white p-1 text-base bg-[#FF5531] rounded-md md:text-lg hover:bg-[#CC4426] m-1" onClick={() => handleCardClick(null)}>Back</button>
+          </div>
+          <div className="w-full">
+            {selectedContent}
+          </div>
+        </div>
+      )}
+      {/* Grid of Boxes
        <div className="-mt-2 p-2 mr-2 lg:mt-0 gap-x-6 grid lg:grid-flow-col grid-flow-row auto-cols-max justify-center gap-4">
         <Box
           description="Explore Tailwind CSS"
@@ -14,7 +55,7 @@ export default function Home() {
           buttonColor="bg-pink-600"
           hoverColor="hover:bg-orange-500"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/app/components/navbarList.js
@@ -0,0 +1,11 @@
+export default function NavbarList() {
+
+    return (
+        <div className="flex flex-col items-center">
+          <h2 className="text-white text-4xl p-2">Navbar</h2>
+          <div className="w-full md:w-2/3 h-96 bg-black">
+
+          </div>
+        </div>
+    )
+}
