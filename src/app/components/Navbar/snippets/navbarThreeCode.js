33cdcb079e5830607fea43d75a1411ecf791b069
Misael Bolainez <misaelbolainez@gmail.com>
2024-08-28 15:05:23 -0700

    feat(component): Added another component to the navbar subpage

---
+++ src/app/components/Navbar/snippets/navbarThreeCode.js
@@ -0,0 +1,53 @@
+const navbarThreeCode = `
+    #navbarThree.js
+
+    "use client";
+
+    import { Component } from "react";
+
+    export default class NavbarThree extends Component {
+      render() {
+        const {
+          logo = "Default Logo",
+          links = ["Home", "About", "Contact"],
+          backgroundColor = "bg-gray-500",
+          textColor = "text-yellow",
+          hoverColor = "hover:text-yellow-500",
+        } = this.props;
+
+        return (
+          <nav className={\`flex justify-between items-center p-4 \${backgroundColor} shadow-lg\`}>
+            <div className="flex space-x-6">
+              {links.map((link, index) => (
+                <a
+                  key={index}
+                  href="#"
+                  className={\`text-lg font-medium \${textColor} \${hoverColor} transition-colors duration-300\`}
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+            <div>
+                <input type="text" placeholder="Search"/>
+            </div>
+            <div className="text-2xl font-extrabold">
+              {logo}
+            </div>
+          </nav>
+        );
+      }
+    }
+
+    #Call component
+    
+    <NavbarThree
+      logo: "YourSite"
+      links: ["Home", "Prices", "Resources"]
+      backgroundColor: "bg-gray-500"
+      textColor: "text-white"
+      hoverColor: "hover:text-yellow-300"
+    />
+`;
+
+export default navbarThreeCode;
