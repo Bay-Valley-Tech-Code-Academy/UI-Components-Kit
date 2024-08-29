5ca903ce78639d19875ed1020f2dc25f6a884f65
AlissonRoss <alirosssalazar@gmail.com>
2024-08-29 16:17:21 -0700

    added bird image to card

--- src/app/components/Card/cardTwo.js
+++ src/app/components/Card/cardTwo.js
@@ -1,5 +1,6 @@
 "use client"
 import { Component } from "react";
+import Image from 'next/image'
 export default class CardTwo extends Component {
   render() {
     const { description = "Default Description", title = "Default Title", button = "Click Me", author = "Default Footer Text", buttonColor = "bg-indigo-600", hoverColor = "hover:bg-indigo-500", focusColor = "focus-visible:outline-indigo-600", width="w-full" } = this.props;
@@ -8,6 +9,7 @@ export default class CardTwo extends Component {
       <div className="-mt-2 p-2 mr-2 lg:mt-0 gap-x-6 grid lg:grid-flow-col grid-flow-row auto-cols-max justify-center gap-4">
         <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 gap-x-6">
           <div className="mx-2 max-w-xs">
+            <Image src="/images/bird.jpg" alt="bird" width={500} height={50}/>
             <p className="mt-6 flex items-baseline justify-center gap-x-2 px-[5rem]">
               <span className="text-5xl font-bold tracking-tight text-gray-900">{title}</span>
             </p>
