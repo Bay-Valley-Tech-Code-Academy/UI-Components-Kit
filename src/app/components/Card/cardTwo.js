4f41c755253e3e1566594c9823c85e96151d162e
AlissonRoss <alisson@alisson.me>
2024-09-03 09:16:38 -0700

    rounded corners of image

--- src/app/components/Card/cardTwo.js
+++ src/app/components/Card/cardTwo.js
@@ -9,7 +9,7 @@ export default class CardTwo extends Component {
       <div className="-mt-2 p-2 mr-2 lg:mt-0 gap-x-6 grid lg:grid-flow-col grid-flow-row auto-cols-max justify-center gap-4">
         <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 gap-x-6">
           <div className="mx-2 max-w-xs">
-            <Image src="/images/bird.jpg" alt="bird" width={500} height={50}/>
+            <Image className='rounded-lg' src="/images/bird.jpg" alt="bird" width={500} height={50}/>
             <p className="mt-6 flex items-baseline justify-center gap-x-2 px-[5rem]">
               <span className="text-5xl font-bold tracking-tight text-gray-900">{title}</span>
             </p>
