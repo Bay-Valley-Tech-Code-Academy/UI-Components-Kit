d8ef7b0aed8e473101419a17c24c9259e34402e2
kxtmg <disizkatie@gmail.com>
2024-09-20 08:22:19 -0700

    Fixed syntax

--- src/app/components/Card/cardFour.js
+++ src/app/components/Card/cardFour.js
@@ -10,24 +10,24 @@ export default function cardFour({image, restaurant, type, price, rating, review
     ]
 
     return (
-<>
-    <div className="flex flex-col bg-white py-16">
-        <div className="flex flex-col md:flex-row shadow-xl m-4 rounded-xl overflow-hidden">
-            <div className="w-full md:w-36 h-40 overflow-hidden">
-                <img 
-                    src={image}
-                    className="w-full h-full object-cover"/>
+        <>
+            <div className="flex flex-col items-center bg-white py-16">
+                <div className="flex flex-col md:flex-row shadow-xl rounded-xl overflow-hidden w-[550px]">
+                    <div className="w-full md:w-36 h-40 overflow-hidden">
+                        <img 
+                            src={image}
+                            className="w-full h-full object-cover"/>
+                    </div>
+                    <div className="flex flex-col gap-1 p-4">
+                        <p className="font-bold text-lg md:text-xl">{restaurant}</p>
+                        <p className="text-sm md:text-base">{type} ({price})</p>
+                        <p className="text-sm md:text-base">{rating} {review} reviews</p>
+                        <button className="bg-orange-500 p-2 w-full md:w-24 rounded-md">
+                            See menu
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col gap-1 p-4">
-                <p className="font-bold text-lg md:text-xl">{restaurant}</p>
-                <p className="text-sm md:text-base">{type} ({price})</p>
-                <p className="text-sm md:text-base">{rating} {review} reviews</p>
-                <button className="bg-orange-500 p-2 w-full md:w-24 rounded-md">
-                    See menu
-                </button>
-            </div>
-        </div>
-    </div>
-</>
+        </>
     )
 }
